e4187e1b1df100d6d2a2f6416a5a0d643d8b251b
Hiteku <ni981320ck@gmail.com>
2024-06-23 18:28:06 +0800

    -

---
+++ tos/number/data.js
@@ -0,0 +1,19 @@
+// e.g. { id: 2598, attr: 'earth', race: 'god', branch: 2 },
+// attr: water fire earth light dark
+// race: god demon human beast dragon elf machina
+const data = [
+  { id: 2863, attr: 'fire', race: 'god', branch: 4 },
+  { id: 2907, attr: 'water', race: 'demon' },
+  // { id: 10427, attr: 'light', race: 'human' },
+  { id: 10495, attr: 'dark', race: 'human' },
+  // { id: 10502, attr: 'water', race: 'elf' },
+  // { id: 10506, attr: 'dark', race: 'elf' },
+  // { id: 10509, attr: 'fire', race: 'human' },
+  { id: 10559, attr: 'dark', race: 'dragon' }, 
+  { id: 10583, attr: 'earth', race: 'human' },
+  { id: 10592, attr: 'fire', race: 'demon' },
+  { id: 10613, attr: 'fire', race: 'machina' },
+  { id: 10647, attr: 'fire', race: 'human' },
+];
+
+export default data;
